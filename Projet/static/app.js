1c4462804a28bcd9ff0e5b06ec10f5ae0f31445b
vdubuisson <va.dubuisson@laposte.net>
2015-05-20 18:53:24 +0200

    Update app.js

--- Projet/static/app.js
+++ Projet/static/app.js
@@ -1,19 +1,12 @@
 var app = angular.module('web', []);
 
-app.controller('WebCtrl', function($scope, $http){
-	$scope.sous_rub = [];
-	$scope.elements = [];
+app.controller('Accueil', function($scope, $http){
 	$http.post('http://localhost:5000/', "").success(function(data, status) {
+		$scope.bienvenue = data[0];
+		data.shift();
 		$scope.rubriques = data;
 		
-		for(i=0; i<$scope.rubriques.length; i++){
-			$scope.sous_rub.push($scope.rubriques[i].sous_rub);
-		}
-		for(i=0; i<$scope.sous_rub.length; i++){
-			$scope.elements.push($scope.sous_rub[i].titre);
-		}		
+				
 	}).error(function(data){
 	});
 });
-
-
