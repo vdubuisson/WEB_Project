6b2289345e9a86b93be1e83501081020dc0158c6
Dubuisson Valentin <vdubuisson@tc405-114-13.insa-lyon.fr>
2015-05-20 11:54:52 +0200

    commit

--- Projet/app.py
+++ Projet/app.py
@@ -8,14 +8,19 @@ app.secret_key = os.urandom(256)
 
 SALT = 'foo#BAR_{baz}^666'
 
-engine = create_engine('sqlite:///base.db', echo=True)
+engine = create_engine('mysql+pymysql://cdcmlyon:projetWeb3tc@sql.free.fr/cdcmlyon', echo=True)
 metadata = MetaData()
 
 Rubriques = Table('Rubriques', metadata,
-    Column('id', Integer, primary_key=true),
+    Column('id', Integer, primary_key=True),
     Column('titre', String, nullable=False),
-    Column('id_parent', Integer),
     Column('id_page', Integer, nullable=False))
+    
+Elements = Table('Elements', metadata,
+	Column('id', Integer, primary_key=true),
+	Column('id_rubrique', Integer, nullable=False),
+	Column('texte', String, nullable=False),
+	Column('lien', String))
 
 def hash_for(password):
 	salted = '%s @ %s' % (SALT, password)
@@ -30,48 +35,17 @@ def index():
 	db = engine.connect()
 	try:
 		data = []
-		row = db.execute(select([Rubriques.c.titre]).where(Rubriques.c.id_page == 1)).fetchall()
-		for title in row:
-			print(title)
-			#data.append({'titre': title})
+		row = db.execute(select([Rubriques.c.id, Rubriques.c.titre]).where(Rubriques.c.id_page == 1)).fetchall()
+		for rubrique in row :
+			elements = []
+			row_elem = db.execute(select([Elements.c.texte, Rubriques.c.lien]).where(Elements.c.id_rubrique == rubrique[0])).fetchall()
+			
+			data.append({'titre': rubrique[1], 'elements': elements})
+		print(data)
+
 	finally:
 		db.close()
-	data = [{'titre': "Exemple"}, {'titre': "Ex 2"}, {'titre': "Ex autre"}]
 	return json.dumps(data)
-	
-	
-	
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
